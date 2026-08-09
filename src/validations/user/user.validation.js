e6e755422efe9b5f0acb505d42b77620af446351
dhananjay-gg <dkhole123@gmail.com>
2026-08-09 23:22:48 +0530

    d

--- src/validations/user/user.validation.js
+++
@@ -1,6 +0,0 @@
-const Joi = require("joi");
-
-exports.updateProfileSchema = Joi.object({
-    full_name: Joi.string().min(3).max(100).required(),
-    phone: Joi.string().pattern(/^[0-9]{10}$/).required()
-});
